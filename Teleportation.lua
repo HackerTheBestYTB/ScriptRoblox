615889f2297816050f8f07d43b047b110c2b5f93
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-04-07 23:27:56 +0700

    Update Teleportation.lua

--- Teleportation.lua
+++ Teleportation.lua
@@ -44,7 +44,7 @@ local dependencies = {
         raycast_params = RaycastParams.new(),
         path = pathfinding_service:CreatePath({WaypointSpacing = 3}),
         player_speed = 150, 
-        vehicle_speed = 700
+        vehicle_speed = 600
     },
     modules = {
         ui = require(replicated_storage.Module.UI),
