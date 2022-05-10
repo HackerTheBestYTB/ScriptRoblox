6c4bd3517afc06d0744b68e3b38fade44f167ff0
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-05-10 12:34:05 +0700

    Update Teleportation.lua

--- Teleportation.lua
+++ Teleportation.lua
@@ -128,7 +128,7 @@ function movement:pathfind(tried)
                     return;
                 end;
 
-                wait(0.25);
+                wait(0.05);
             end;
         end;
 
@@ -145,7 +145,7 @@ function movement:move_to_position(part, cframe, speed, car, target_vehicle, tri
     
     if not car and workspace:Raycast(part.Position, dependencies.variables.up_vector, dependencies.variables.raycast_params) then -- if there is an object above us, use pathfind function to get to a position with no collision above
         movement:pathfind({});
-        wait(0.05);
+        wait(0.5);
     end;
     
     local y_level = 600;
