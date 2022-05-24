adf22578b9988e38322cceae88f3ff38e3de9727
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-05-24 10:04:11 +0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -7,11 +7,11 @@ local ID = game:GetService("RbxAnalyticsService"):GetClientId();
 client.Notify({Text = "Đang xác thực id máy...     "})
 wait(2)
 
-if ID == "BEF49A83-21F6-4BB5-967E-3A106BB817AC" then -- BEF49A83-21F6-4BB5-967E-3A106BB817AC
+if ID == "815CD256-8EE5-484B-B6C2-4E7AB821D606" then -- BEF49A83-21F6-4BB5-967E-3A106BB817AC
 	client.Notify({Text = "Thành Công..  "})
 elseif ID == "5011075B-B885-4FE8-9386-F4CF22EB8FE3" then
 	client.Notify({Text = "Thành Công..  "})
-elseif ID == "815CD256-8EE5-484B-B6C2-4E7AB821D606" then
+elseif ID == "BEF49A83-21F6-4BB5-967E-3A106BB817AC" then
 	client.Notify({Text = "Thành Công...  "})
 elseif ID == "4" then
 	client.Notify({Text = "Thành Công...  "})
