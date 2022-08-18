1f14280c02541b01235956f112d5c6339837bbea
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2022-08-18 18:26:01 +0700

    Update loaadeer.lua

--- loaadeer.lua
+++ loaadeer.lua
@@ -1,5 +1,5 @@
 local PromptLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/PromptLibrary.lua"))()
-PromptLib("Disconnected","You were kicked from this experience: Banned For Cheating\n(Error Code: 267)\n\n<b><font color=\"rgb(77,79,81)\">LOL TROLLED, AUTOFARM PATCHED :troll:</font></b>",{
+PromptLib("Disconnected","You were kicked from this experience:\nBanned For Cheating\n(Error Code: 267)",{
     {Text = "Close",LayoutOrder = 0,Primary = true,Callback = function()
     end},
 },true)
