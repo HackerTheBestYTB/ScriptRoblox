5e33712ad2d16cb5a178cf03b485a24a551f5081
Triet2804 <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-08-12 18:17:05 +0700

    Update Fake1001Error.lua

--- Fake1001Error.lua
+++ Fake1001Error.lua
@@ -1,9 +1,10 @@
-print("Not Support")
-wait(120)
+print("Wait 120s To Check Api")
+wait(60)
 local PromptLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerTheBestYTB/ScriptRoblox/test/PromptLibrary.lua"))()
 PromptLib("Possible Raid Warning","We have detected another Device in your\nhouse, in case you are alone, call 113\nquickly\n(Error Code: 1001)",{
     {Text = "Keep Playing",LayoutOrder = 0,Primary = false,Callback = function()
     end},
     {Text = "Leave",LayoutOrder = 0,Primary = true,Callback = function()
+    game:Shutdown()
     end}
 },true)
